5560329ad67d5d6020a323eb80d21d8457bee29e
itstalhaiqbal <ti709888@gmail.com>
2025-06-10 17:32:42 +0500

    first commit

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://talha:spidy@cluster0.bhnmkqd.mongodb.net/invoice?retryWrites=true&w=majority&appName=Cluster0";
+const MONGODB_URI =  "mongodb+srv://talha:spidy@cluster0.bhnmkqd.mongodb.net/invoice?retryWrites=true&w=majority&appName=Cluster0";
 
 if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable');
